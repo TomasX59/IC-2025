c67a1dab836ec46ed95b649e4318881671ddca10
Daniel Calonico <daniel.calonico@icloud.com>
2025-11-10 16:56:10 +0000

    added image color channel extractor

---
+++ lab-work-2/src/imageColorChannel.cpp
@@ -0,0 +1,44 @@
+#include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+using namespace cv;
+
+// Using OpenCv
+// extract a single color channel from image
+// args:
+// file_name
+// channel_number (0 = R, 1 = G, 2 = B)
+int main(const int argc, char *argv[])
+{
+    if (argc < 2)
+    {
+        cerr << "Usage: colorChannel file_name channel_number{ 0 | 1 | 2 }\n";
+        cerr << "    channel_number (0 = R, 1 = G, 2 = B)\n";
+        return 1;
+    }
+
+    const string file_path = argv[1];
+    const int channel_number = atoi(argv[2]);
+    const Mat img = imread(file_path, IMREAD_COLOR_RGB);
+
+    if (img.empty())
+    {
+        cout << "Could not read the image: " << file_path << endl;
+        return 1;
+    }
+
+    Mat channel;
+    const string extension = file_path.substr(file_path.find_last_of('.') + 1);
+    const string new_path = file_path + "_" + to_string(channel_number) + "." + extension;
+    extractChannel(img, channel, channel_number);
+
+    imshow("Display window", channel);
+    imwrite(new_path, channel);
+
+    return 0;
+}
--- lab-work-2/src/imageProcessor.cpp
+++ lab-work-2/src/imageProcessor.cpp
@@ -1,30 +1,43 @@
- 
 #include <opencv2/core.hpp>
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/highgui.hpp>
- 
+
 #include <iostream>
- 
+
 using namespace cv;
- 
-int main()
+using namespace std;
+
+// Without using existing specific functions on OpenCv
+// -n create negative image
+// -y mirror image vertically
+// -x mirror image horizontally
+// -l <degrees> rotate image by multiples of 90º
+// -b <intensity> increase brightness
+// -d <intensity> decreases brightness
+int main(int argc, char *argv[])
 {
+    if (argc < 1)
+    {
+        std::cerr << "Usage: bin2text image_file text_file\n";
+        return 1;
+    }
+
     std::string image_path = samples::findFile("starry_night.jpg");
     Mat img = imread(image_path, IMREAD_COLOR);
- 
-    if(img.empty())
+
+    if (img.empty())
     {
         std::cout << "Could not read the image: " << image_path << std::endl;
         return 1;
     }
- 
+
     imshow("Display window", img);
     int k = waitKey(0); // Wait for a keystroke in the window
- 
-    if(k == 's')
+
+    if (k == 's')
     {
         imwrite("starry_night.png", img);
     }
- 
+
     return 0;
 }
